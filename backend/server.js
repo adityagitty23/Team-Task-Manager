3702d42b63c042ee99fbc0d954631ebb63daf9c8
adityagitty23 <aditya3250a@gmail.com>
2026-05-21 23:11:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin:
+      "https://team-task-manager-three-beta.vercel.app/",
+    credentials: true,
+  })
+)
 app.use(express.json())
 
 mongoose
